4b4379475e3cea5efb9170b64baa9e55015c7c58
Nccchan <cima_6000@icloud.com>
2025-04-04 07:19:54 +0900

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ShippingEstimator from './App.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ShippingEstimator />
-  </React.StrictMode>
-);
