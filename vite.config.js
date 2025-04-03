e1ff12e8cc7d3dc4157bd3c5fb820ee06f61340a
Nccchan <cima_6000@icloud.com>
2025-04-04 07:12:18 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  build: {
-    rollupOptions: {
-      input: 'index.html'
-    }
-  }
+  base: '/'
 })
